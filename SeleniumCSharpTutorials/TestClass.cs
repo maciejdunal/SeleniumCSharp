82ba4ad8e1465f66e7025eced3d843e1ea98f21b
Maciej Dunal <maciejdunal@gmail.com>
2022-02-16 19:14:20 +0100

    Selenium C# .NET NUnit Tutorial-01 Completed.

--- SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharpTutorials/TestClass.cs
@@ -3,18 +3,41 @@
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumCSharpTutorials.BaseClass;
+using System.Threading;
 
 namespace SeleniumCSharpTutorials
 {
     [TestFixture]
-    public class TestClass
+    public class TestClass : BaseTest
     {
         [Test]
-        public void TestMethod()
+        public void TestMethod1()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            IWebElement acceptCookies = driver.FindElement(By.XPath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]"));
+            acceptCookies.Click();
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
         }
     }
 }
+ 
